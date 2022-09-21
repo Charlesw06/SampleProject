a39b0faeef97782eded7327583665e1fd6c299d7
Charlesw06 <cwalford3884@bths.edu>
2022-09-20 20:01:52 -0400

    Added print statement for favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Charles Walford");
         System.out.println("Avengers: Infinity War");
+        System.out.println("Pizza");
     }
 }
